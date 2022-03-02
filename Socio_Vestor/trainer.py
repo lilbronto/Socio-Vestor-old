d9dd145f9e3685eeabf31a11b912d3478c3f066e
Moritz Bewerunge <mobewerunge@gmail.com>
2022-03-02 09:43:25 +0100

    Added a trainer file, a utils file and a params file

---
+++ Socio_Vestor/trainer.py
@@ -0,0 +1,76 @@
+from Socio_Vestor.utils import compute_rmse
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+import joblib
+from google.cloud import storage
+
+from Socio_Vestor.params import BUCKET_NAME, STORAGE_LOCATION
+
+
+
+class Trainer():
+
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+        self.experiment_name = "[GER] [MUC] [socio-vestor] socio_vestor_783"
+
+    def set_pipeline(self):
+        pass
+
+    def run(self):
+        """set and train the pipeline"""
+        self.pipeline = self.set_pipeline().fit(self.X,self.y)
+        return self.pipeline
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        print(f"The root mean squared error is: {rmse}")
+        return rmse
+
+    # MLFLOW CLIENT
+    @memoized_property
+    def mlflow_client(self):
+        MLFLOW_URI = "https://mlflow.lewagon.co/"
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+    # Saving the Model
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, 'model.joblib')
+
+        # Implement here
+        client = storage.Client()
+
+        bucket = client.bucket(BUCKET_NAME)
+
+        blob = bucket.blob(STORAGE_LOCATION)
+
+        blob.upload_from_filename('model.joblib')
+        print(f"uploaded model.joblib to gcp cloud storage under \n => {STORAGE_LOCATION}")
