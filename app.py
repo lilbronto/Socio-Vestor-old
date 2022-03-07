2ebea2d7d5e9bf392c6af6b71c21a84d7dd993da
Moritz Bewerunge <mobewerunge@gmail.com>
2022-03-07 14:33:34 +0100

    Created the add file for streamlit and edited the makefile accordingly

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import numpy as np
+import datetime
+import joblib
+
+from Socio_Vestor.data import get_main_df
+from Socio_Vestor.preprocessing import impute_data
+
+'''
+Get the timeframe for the Price data that will be used to make our prediction
+'''
+st.write(f"Pleas select the timeframe for your prediction")
+start_date = st.date_input(
+    "Start Date:",
+    datetime.date(2022, 3, 3))
+# end_date = st.date_input(
+#     "End Date:",
+#     datetime.date(2022, 3, 11))
+
+
+'''
+Make an API Call to fetch the data we need for our prediction
+'''
+# Get the data
+data = get_main_df()
+X_pred = data.loc[start_date:]
+X_pred_imp = impute_data(X_pred) # Change Imputer accordingly
+X_test = X_pred_imp.drop(['price_high', 'price_low', 'price_close']) # drop the columns we did not use
+# preprocess the data?
+
+
+# load the trained model
+model = joblib.load('sociovestor.joblib')
+
+# calculate a prediction
+y_pred = model.predict(X_test)
+y_pred_array = np.array(np.reshape(y_pred,y_pred.shape[0]))
+
+# Plot the predicted values on streamlit website
+st.line_chart(y_pred_array)
