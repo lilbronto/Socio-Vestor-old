04161a9315cfff4d6ef4d38cb775ea703c7da48a
Dayus Madhan <dayusmadhan@gmail.com>
2022-03-08 17:03:18 +0100

    updated trainer for simplernn_main

--- Socio_Vestor/trainer.py
+++ Socio_Vestor/trainer.py
@@ -1,13 +1,15 @@
 from Socio_Vestor.utils import compute_rmse
-from memoized_property import memoized_property
-import mlflow
-from  mlflow.tracking import MlflowClient
+# from memoized_property import memoized_property
+# import mlflow
+# from  mlflow.tracking import MlflowClient
 import joblib
 from google.cloud import storage
 
 from Socio_Vestor.params import BUCKET_NAME, STORAGE_LOCATION
 
-from Socio_Vestor.models import LayerLSTM, SimpleRnn, LSTM
+from Socio_Vestor.models import LayerLSTM, SimpleRNN_main, LSTM
+
+
 
 class Trainer():
 
@@ -17,7 +19,7 @@ class Trainer():
             y: pandas Series
         """
 
-        self.experiment_name = "[GER] [MUC] [socio-vestor] socio_vestor_783"
+        # self.experiment_name = "[GER] [MUC] [socio-vestor] socio_vestor_783"
 
 
     def evaluate(self, model, X_test, y_test):
@@ -32,28 +34,28 @@ class Trainer():
     # Saving the Model
     def save_model(self, model):
         """ Save the trained model into a model.joblib file """
-        joblib.dump(model, 'sociovestor.joblib')
+        joblib.dump(model, 'simplernn_main.joblib')
 
 
 if __name__ == "__main__":
 
     # import your model, get the train and test data and train it
-    model = LayerLSTM()
+    model = SimpleRNN_main()
     X_train, X_test, y_train, y_test = model.get_data()
-    model.train_LSTM(X_train, y_train)
+    model.train_SimpleRNN(X_train, y_train)
 
     # initialize a trainer and use it to save the model in the cloud
     trainer = Trainer()
     trainer.save_model(model.model)
 
     # evaluate the model and print its score
-    evaluated_model = trainer.evaluate(model, X_test, y_test)
+    # evaluated_model = trainer.evaluate(model, X_test, y_test)
 
     # use mlflow to keep track of the hyperparameters
-    trainer.mlflow_log_metric("rmse", evaluated_model)
-    trainer.mlflow_log_param("model", "LSTM with four Layers")
-    trainer.mlflow_log_param("student_name", trainer.experiment_name)
+    # trainer.mlflow_log_metric("rmse", evaluated_model)
+    # trainer.mlflow_log_param("model", "LSTM with four Layers")
+    # trainer.mlflow_log_param("student_name", trainer.experiment_name)
 
     # print the website where the hyperparameters can be found
-    experiment_id = trainer.mlflow_experiment_id
-    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
+    # experiment_id = trainer.mlflow_experiment_id
+    # print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
